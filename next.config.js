92f0a938cadcb7e3c2f66e490b9af82f913e0518
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-16 21:47:59 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,6 +19,26 @@ const nextConfig = {
           },
         ],
       },
+       {
+        source: "/api/products/:path*", // Match any API route
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your frontend origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  }, 
+  
       {
         source: "/api/follow/:path*", // Match any API route
         headers: [
