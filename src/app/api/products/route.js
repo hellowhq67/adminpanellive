1ca5ebdd325c667fe8c82344b8a5ff060be0140a
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-20 00:42:21 +0600

    Update route.js

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,18 +1,23 @@
 
 import { NextResponse } from 'next/server';
-
+import { parse } from 'url';
 import Product from '@/models/prouduct'; // Import the Product model
 import connectMongoDB from '@/db/db';
-export async function GET() {
+export async function GET(request) {
   try {
     await connectMongoDB(); // Connect to your MongoDB database
 
-    const products = await Product.find({}); // Retrieve all products from the database
+    const { query } = parse(request.url, true);
+    const { page = 1, limit = 10 } = query; // Default page is 1 and limit is 10, you can adjust as needed
 
-    return NextResponse.json({ products }); // Return the products as JSON response
+    const skip = (parseInt(page) - 1) * parseInt(limit);
+
+    const products = await Product.find({}).skip(skip).limit(parseInt(limit)); // Skip certain number of items and limit the number of items per page
+
+    return NextResponse.json({ products });
   } catch (error) {
     console.error("Error fetching products:", error);
-    return NextResponse.error(error); // Return an error response if there's an error
+    return NextResponse.error(error);
   }
 }
 export async function POST(request) {
