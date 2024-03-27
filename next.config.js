fa033ed7669ead1248c6b1ea52f7cee56c7685ee
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-27 23:01:14 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,79 +3,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*", // Match any API route
+        source: "/(api|:path*)", // Match any route including /api and others
         headers: [
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://finalversion-of-grailed-front-end.vercel.app/", // Set your frontend origin
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-      {
-        source: "/api/products/:path*", // Match any API route
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Set your frontend origin
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-        {
-        source: "/api/products/total/:path*", // Match any API route
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Set your frontend origin
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-              {
-        source: "/api/blog/:path*", // Match any API route
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Set your frontend origin
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-        ],
-      },
-      {
-        source: "/api/contact/:path*", // Match any API route
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "*", // Set your frontend origin
+            value: "https://finalversion-of-grailed-front-end.vercel.app", // Allow requests from any origin
           },
           {
             key: "Access-Control-Allow-Methods",
