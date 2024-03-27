44ef94e690b1e311d878e29314ccf8c7e60cf566
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-28 00:14:04 +0600

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,24 +1,18 @@
 import mongoose from "mongoose";
 
-
 const connectMongoDB = async () => {
   try {
     await mongoose.connect("mongodb+srv://floorapp355:J1YmCDnTvZsAvGmt@floorapp.ysgurtu.mongodb.net/my-app", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       // Additional options
-      useFindAndModify: false, // Set to false to use native findOneAndUpdate() instead of findAndModify()
-      useCreateIndex: true, // Ensure indexes are created when defining schemas
-      autoIndex: false, // Set to false to disable automatic index creation (only use in production if you manage indexes manually)
-      poolSize: 10, // Maximum number of connections in the connection pool
-      socketTimeoutMS: 30000, // How long the MongoDB driver waits before timing out a query
-      keepAlive: true, // Whether to use TCP keep-alive
-      connectTimeoutMS: 30000, // How long the MongoDB driver waits before timing out a connection attempt
+      serverSelectionTimeoutMS: 5000, // Timeout for server selection when connecting
+      socketTimeoutMS: 45000, // How long the MongoDB driver waits before timing out a query
     });
     console.log("Connected to MongoDB.");
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
   }
 };
-export default connectMongoDB;
 
+export default connectMongoDB;
