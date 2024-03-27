5ceaa2cff8ff4ae44d1fb0da782f2d2773d2fe68
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-28 02:24:21 +0600

    Update route.js

--- src/app/api/total/route.js
+++ src/app/api/total/route.js
@@ -15,3 +15,63 @@ export async function GET() {
     return NextResponse.error(error); // Return an error response if there's an error
   }
 }
+export async function POST(request) {
+  try {
+    await connectMongoDB();
+
+    const {
+      userId,
+      userName,
+      shippings,
+      productImage1,
+      productImage2,
+      productImage3,
+      productImage4,
+      productImage5,
+      designers,
+      productName,
+      size,
+      color,
+      price,
+      floorPrice,
+      description,
+      vendor,
+      condition,
+      department,
+      category,
+      subcategory,
+      tag,
+      acceptOffer,
+    } = await request.json();
+
+    const newProduct = await Product.create({
+      userId,
+      userName,
+      shippings,
+      productImage1,
+      productImage2,
+      productImage3,
+      productImage4,
+      productImage5,
+      designers,
+      productName,
+      size,
+      color,
+      price,
+      floorPrice,
+      description,
+      vendor,
+      condition,
+      department,
+      category,
+      subcategory,
+      tag,
+      acceptOffer,
+    });
+
+    return NextResponse.json({ message: "Product Created", product: newProduct }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating product:", error);
+    return NextResponse.error(error);
+  }
+} 
