67203b606b973e7288aa59b8717d6ba75e9cea18
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-27 17:02:45 +0600

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://floorapp355:J1YmCDnTvZsAvGmt@floorapp.ysgurtu.mongodb.net/my-app");
+    await mongoose.connect("mongodb+srv://floorapp355:J1YmCDnTvZsAvGmt@floorapp.ysgurtu.mongodb.net/my-app", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // Add any additional options here if needed
+    });
     console.log("Connected to MongoDB.");
   } catch (error) {
-    console.log(error);
+    console.error("Failed to connect to MongoDB:", error);
   }
 };
 
