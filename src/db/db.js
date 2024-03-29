9d87fb95a3cc6d94a0270ced01a0e5daddfa55fe
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-30 03:41:36 +0600

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -5,8 +5,7 @@ const connectMongoDB = async () => {
     await mongoose.connect("mongodb+srv://floorapp355:J1YmCDnTvZsAvGmt@floorapp.ysgurtu.mongodb.net/my-app?retryWrites=true&w=majority&appName=floorapp", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false, // Disable deprecated findOneAndUpdate
-      useCreateIndex: true // Ensure indexes are built
+      useCreateIndex: true// Ensure indexes are built
     });
     
     console.log("Connected to MongoDB.");
