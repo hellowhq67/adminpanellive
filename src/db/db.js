6899ddf81d3d719ed9ffac68db3b4d08bd0a5004
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-30 03:38:17 +0600

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -2,11 +2,16 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://floorapp355:J1YmCDnTvZsAvGmt@floorapp.ysgurtu.mongodb.net/my-app?retryWrites=true&w=majority&appName=floorapp");
+    await mongoose.connect("mongodb+srv://floorapp355:J1YmCDnTvZsAvGmt@floorapp.ysgurtu.mongodb.net/my-app?retryWrites=true&w=majority&appName=floorapp", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false, // Disable deprecated findOneAndUpdate
+      useCreateIndex: true // Ensure indexes are built
+    });
     
     console.log("Connected to MongoDB.");
   } catch (error) {
-    console.log(error);
+    console.error("Error connecting to MongoDB:", error);
   }
 };
 
