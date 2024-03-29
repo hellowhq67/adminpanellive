057dd80cbf307a9b93355f16b115a66d990a4c8f
Lijan Haque <93515488+hellowhq67@users.noreply.github.com>
2024-03-29 12:35:51 +0600

    Update db.js

--- src/db/db.js
+++ src/db/db.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 const connectMongoDB = async () => {
   try {
     await mongoose.connect("mongodb+srv://floorapp355:J1YmCDnTvZsAvGmt@floorapp.ysgurtu.mongodb.net/my-app?retryWrites=true&w=majority&appName=floorapp", {
-      useNewUrlParser: true,
+        useNewUrlParser: true,
       useUnifiedTopology: true,
+      poolSize: 10, // Adjust pool size as needed
+      retryWrites: true, // Enable retryable writes
       // Additional options
       serverSelectionTimeoutMS: 5000, // Timeout for server selection when connecting
-      socketTimeoutMS: 45000, // How long the MongoDB driver waits before timing out a query
+      socketTimeoutMS: 45000 // How long the MongoDB driver waits before timing out a query
     });
     console.log("Connected to MongoDB.");
   } catch (error) {
